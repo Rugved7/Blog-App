0821b4eba384bc09cf3ebda7106e2b4b3f3c76d9
Rugved7 <rugvedagasti7@gmail.com>
2024-06-08 02:18:25 +0530

    Worked on Components Section

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -1,21 +1,20 @@
 import React from "react";
-import { useDispatch } from "react-redux";
 import authService from "../../appwrite/auth";
 import { logout } from "../../store/authSlice";
+import { useDispatch } from "react-redux";
 
 const LogoutBtn = () => {
   const dispatch = useDispatch();
   const logoutHandler = () => {
     authService.logout().then(() => {
-      dispatch(logout());
+      dispatch(logout); // making sure that the store gets updated always
     });
   };
+
   return (
-    <div>
-      <button className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full">
-        Logout
-      </button>
-    </div>
+    <button className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full">
+      Logout
+    </button>
   );
 };
 
